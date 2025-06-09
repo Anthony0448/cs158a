39c60b97855f039f69757ef62645f993b5c47efb
anthony <anthonyflores5844@gmail.com>
2025-06-09 12:21:56 -0700

    myserver.py serverSocket.bind only takes one argument, so parentheses was added

--- sockets/myserver.py
+++ sockets/myserver.py
@@ -7,10 +7,11 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 
 # Bind socket to the port
 # The empty string means the server will listen on all available interfaces
-serverSocket.bind('', serverPort())
+serverSocket.bind(('', serverPort))
 
 # Listen for (one) incoming connections
 serverSocket.listen(1)
+print("Listening...")
 
 # Accept
 # Accept connection from a client
